22d9e05466eac31fefa9a4c321023bee5b4528ff
bellinhajoaninha <00001106685854sp@al.educacao.sp.gov.br>
2024-05-27 18:52:30 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(1600, 800);
+  background("black");
+}
+
+function draw() {
+ 
+  
+  stroke("pink");
+  fill("purple");
+  
+  // console.log(mouseIsPressed);
+  
+  if (mouseIsPressed){ 
+  circle(mouseX, mouseY, 30);
+  }
+  
+  
+  
+
+
+
+
+
+
+
+}
